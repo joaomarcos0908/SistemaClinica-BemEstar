dadccecbe0ec5a10dc35b738afbb7206ba579f28
João Marcos Cavalcanti <joaomarcosnog10@gmail.com>
2025-11-16 21:32:38 -0300

    Feat: Adicionando metodos de escrita e leitura de arquivo na Classe RepositorioHorario

--- src/RepositorioHorario.java
+++ src/RepositorioHorario.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
@@ -35,7 +36,54 @@ public List<Horario> listarQuadroDeHorarios(){
         }
         return false;
     }
+    public void salvarCSV(String nomeArquivo) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(nomeArquivo))) {
+            bw.write("id,data,disponivel,horaInicio,horaFim,idMedico,tipo");
+            bw.newLine();
+
+            for (Horario h : repositorio) {
+                bw.write(
+                        h.getId() + "," +
+                                h.getData() + "," +
+                                h.isDisponivel() + "," +
+                                h.getHoraInicio() + "," +
+                                h.getHoraFim() + "," +
+                                h.getIdMedico() + "," +
+                                h.getTipo()
+                );
+                bw.newLine();
+            }
+            System.out.println(" Horários salvos em " + nomeArquivo);
+        } catch (IOException e) {
+            System.out.println(" Erro ao salvar horários: " + e.getMessage());
+        }
+    }
+
+    public void carregarCSV(String nomeArquivo) {
+        repositorio.clear();
+        try (BufferedReader br = new BufferedReader(new FileReader(nomeArquivo))) {
+            br.readLine();
+            String linha;
+            while ((linha = br.readLine()) != null) {
+                String[] p = linha.split(",");
+                if (p.length >= 7) {
+                    int id = Integer.parseInt(p[0]);
+                    LocalDate data = LocalDate.parse(p[1]);
+                    boolean disponivel = Boolean.parseBoolean(p[2]);
+                    LocalDateTime horaInicio = LocalDateTime.parse(p[3]);
+                    LocalDateTime horaFim = LocalDateTime.parse(p[4]);
+                    int idMedico = Integer.parseInt(p[5]);
+                    String tipo = p[6];
+                    repositorio.add(new Horario(id, data, disponivel, horaFim, horaInicio, idMedico, tipo));
+                }
+            }
+            System.out.println(" Horários carregados de " + nomeArquivo);
+        } catch (IOException e) {
+            System.out.println("Arquivo de horários não encontrado, iniciando vazio.");
+        }
+    }
 }
+
 
 
 
