7b42d4f56bd5876463fb6826b257cce412c3ba0a
Antoni0Paiva <antonioahoy@gmail.com>
2025-11-16 21:28:36 -0300

    refactor: adiciona enums para TipoPaciente e TipoAtendimento

--- src/Paciente.java
+++ src/Paciente.java
@@ -3,13 +3,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Paciente extends  Pessoa {
-    private String tipoPaciente;
+    private TipoPaciente tipoPaciente;
     private List<String> historicoIds;
-    private String tipoAtendimento;
+    private TipoAtendimento tipoAtendimento;
 
     public Paciente(String nome, String cpf, LocalDate dataNascimento, String email, String numTelefone,
                     String endereco, boolean isGestante, boolean hasEspectroAutista, boolean isPcd, boolean isLactante,
-                    boolean hasCriancaColo, int tipoPaciente, List<String> historicoIds, int tipoAtendimento) {
+                    boolean hasCriancaColo, TipoPaciente tipoPaciente, List<String> historicoIds, TipoAtendimento tipoAtendimento) {
         super(nome, cpf, dataNascimento, email, numTelefone, endereco, isGestante, hasEspectroAutista, isPcd,
                 isLactante, hasCriancaColo);
         setTipoPaciente(tipoPaciente);
@@ -17,45 +17,26 @@ public class Paciente extends  Pessoa {
         setTipoAtendimento(tipoAtendimento);
     }
 
-    public String getTipoPaciente() {
+    public TipoPaciente getTipoPaciente() {
         return tipoPaciente;
     }
 
-    public void setTipoPaciente(int tipoPaciente) {
-        switch (tipoPaciente) {
-            case 1:
-                this.tipoPaciente = "Emergência";
-                break;
-            case 2:
-                if (isPrioridade()) {
-                    this.tipoPaciente = "Prioridade";
-                } else {
-                    throw new IllegalArgumentException("Você não preenche os requisitos para ter um " +
-                            "atendimento prioritário.");
-                }
-                break;
-            case 3:
-                this.tipoPaciente = "Eletivo";
-                break;
-            default:
-                throw new IllegalArgumentException("Tipo de paciente inválido.");
+    public void setTipoPaciente(TipoPaciente tipoPaciente) {
+        if (tipoPaciente == tipoPaciente.PRIORIDADE && !isPrioridade()) {
+            throw new IllegalArgumentException("Paciente não possui os requisitos para um atendimento prioritário.");
         }
+        this.tipoPaciente = tipoPaciente;
     }
-
-    public String getTipoAtendimento() {
-        return tipoAtendimento;
-    }
-
-    public void setTipoAtendimento(int tipoAtendimento) {
-        switch (tipoAtendimento) {
+    public static TipoPaciente obterTipoPacientePorNumero(int opcao) {
+        switch (opcao) {
             case 1:
-                this.tipoAtendimento = "Convênio";
-                break;
+                return TipoPaciente.EMERGENCIA;
             case 2:
-                this.tipoAtendimento = "Particular";
-                break;
+                return TipoPaciente.PRIORIDADE;
+            case 3:
+                return TipoPaciente.ELETIVO;
             default:
-                throw new IllegalArgumentException("Tipo de atendimento inválido.");
+                throw new IllegalArgumentException("Tipo de paciente inválido. Escolha 1, 2 ou 3.");
         }
     }
 
@@ -70,13 +51,15 @@ public class Paciente extends  Pessoa {
             this.historicoIds = new ArrayList<>(historicoIds);
         }
     }
-    public void adicionarHistoricoId(String id){
-        if(id != null && !id.trim().isEmpty()){
+
+    public void adicionarHistoricoId(String id) {
+        if (id != null && !id.trim().isEmpty()) {
             this.historicoIds.add(id);
         }
     }
-    public void verHistoricoId(){
-        if(this.historicoIds.isEmpty()){
+
+    public void verHistoricoId() {
+        if (this.historicoIds.isEmpty()) {
             System.out.println("Histórico vazio!");
         } else {
             for (String id : historicoIds) {
@@ -85,14 +68,33 @@ public class Paciente extends  Pessoa {
         }
     }
 
-    public Boolean isAcompNecessario() {
+    public TipoAtendimento getTipoAtendimento() {
+        return tipoAtendimento;
+    }
+
+    public void setTipoAtendimento(TipoAtendimento tipoAtendimento) {
+        this.tipoAtendimento = tipoAtendimento;
+    }
+    public static TipoAtendimento obterTipoAtendimentoPorNumero(int opcao) {
+        switch (opcao) {
+            case 1:
+                return TipoAtendimento.CONVENIO;
+            case 2:
+                return TipoAtendimento.PARTICULAR;
+            default:
+                throw new IllegalArgumentException("Tipo de atendimento inválido. Escolha 1 ou 2.");
+        }
+    }
+
+
+    public Boolean isAcompanhanteNecessario() {
         return getIdade() < 12;
     }
 
     public Boolean isPrioridade() {
         return getIdade() >= 60 || isGestante() || isPcd() || isLactante() || hasEspectroAutista() || hasCriancaColo();
     }
-
-
-
 }
+
+
+
